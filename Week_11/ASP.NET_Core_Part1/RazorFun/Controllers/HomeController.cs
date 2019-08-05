905b4ba505d3546c524a939b313d7f82c549b067
eric fang <ericfang1992@gmail.com>
2019-08-05 14:49:16 -0700

    introduction to razor assignment

---
+++ Week_11/ASP.NET_Core_Part1/RazorFun/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
