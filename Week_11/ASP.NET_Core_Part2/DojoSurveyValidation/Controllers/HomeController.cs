67731ac01131461b19500e06843c08d126d5a2dd
eric fang <ericfang1992@gmail.com>
2019-08-06 15:37:28 -0700

    Validation with forms

---
+++ Week_11/ASP.NET_Core_Part2/DojoSurveyValidation/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyValidation.Models;
+
+namespace DojoSurveyValidation.Controllers
+{
+    public class HomeController : Controller
+    {   
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProccessSurvey(Survey formData)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Success", formData);
+            } else {
+                return View("Index");
+            }
+        }
+
+
+
+    }
+}
---
+++ Week_11/ASP.NET_Core_Part2/DojoSurveyValidation/Models/Survey.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations; 
+namespace DojoSurveyValidation.Models
+{
+    public class Survey
+    {
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string Language { get; set; }
+
+        [MaxLength(20)]
+        public string Comment { get; set; }
+    }
+}
